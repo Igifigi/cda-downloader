f6a29c6589c70858f61d13221561d32e2d7bac52
Igifigi <ignacyswiderski@wp.pl>
2022-07-27 13:10:07 +0200

    First commit

---
+++ background.js
@@ -0,0 +1,41 @@
+﻿var video_data = [];
+
+// var video_data = {
+//   title: "",
+//   url: "",
+// };
+
+// (1) Uruchamia się po kliknięciu w ikonę rozszerzenia
+chrome.browserAction.onClicked.addListener(function (tab) {
+  //DEBUG
+  //console.log("uruchomil sie krok 1");
+  chrome.windows.getCurrent(function (current_window) {
+    //console.log(current_window);
+    chrome.tabs.query(
+      { active: true, windowId: current_window.id },
+      function (active_tabs) {
+        //console.log(active_tabs);
+        chrome.tabs.executeScript(active_tabs[0].id, {
+          file: "script.js",
+          allFrames: true,
+        });
+      }
+    );
+  });
+  DownloadVideo(video_data[1], video_data.title[0]);
+});
+
+// (2) Uruchamia się po otrzymaniu danych o filmie
+chrome.extension.onRequest.addListener(function (data) {
+  //console.log("otrzymalem dane");
+  DownloadVideo(data.url, data.title);
+});
+
+// (3) Uruchamia się po wywołaniu [punktu 1.]
+function DownloadVideo(video_url, video_filename) {
+  //console.log(video_url, video_filename);
+  chrome.downloads.download({
+    url: video_url,
+    filename: video_filename + ".mp4",
+  });
+}
---
+++ script.js
@@ -0,0 +1,57 @@
+﻿function GetVideoTitle() {
+  var outer_html = document.body.outerHTML;
+  var as_regex = new RegExp("(tiptitle=)(\"|\')[^>]*", "gi");
+  var titles = outer_html.match(as_regex) || [];
+  titles = titles.map(function (s) {
+    return s.substring(10, s.length - 1);
+  });
+
+  // regex to find tiptiles
+  // (tiptitle=)("|')[^>]*
+  // HAS to be translated into js regex
+
+  //   var index_of_title = outer_html.indexOf("tiptitle") + 10;
+  //   console.log("index of title (+10) ", index_of_title);
+  //   var title = "";
+  //   while (
+  //     outer_html[index_of_title] != '"' &&
+  //     (outer_html[index_of_title + 1] != "," ||
+  //       outer_html[index_of_title + 1] != ">")
+  //   ) {
+  //     title += outer_html[index_of_title];
+  //     index_of_title++;
+  //   }
+  return titles[0];
+}
+
+function GetUrlToDownload() {
+  var inner_html = document.body.innerHTML;
+  var as_regex = new RegExp(
+    "(\"|')(http|https)://[^(\"|'|,)]*?.(mp4)[^(\"|')]*?(\"|')",
+    "gi"
+  );
+  var urls = inner_html.match(as_regex) || [];
+  urls = urls.map(function (quoted_string) {
+    return quoted_string.substring(1, quoted_string.length - 1);
+  });
+  return urls[0];
+  //   // regex to find mp4 files
+  //   //  ("|')(http|https):\/\/[^("|'|,)]*?\.(mp4)[^("|')]*?("|')
+  //   // HAS to be translated into js regex
+  //   var end_index_of_url = inner_html.indexOf(".mp4") + 3;
+  //   var start_index_of_url = end_index_of_url - 94;
+  //   var url = "";
+  //   while (start_index_of_url <= end_index_of_url) {
+  //     url += inner_html[start_index_of_url];
+  //     start_index_of_url++;
+  //   }
+  //   return url;
+}
+
+var data = {
+  title: GetVideoTitle(),
+  url: GetUrlToDownload(),
+};
+
+//console.log("wysylam dane");
+chrome.extension.sendRequest(data);
